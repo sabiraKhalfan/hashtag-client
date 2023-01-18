abd54e79ed7e3d48ae05718a8aadc7a33fb58856
sabiraKhalfan <109642751+sabiraKhalfan@users.noreply.github.com>
2023-01-18 13:27:57 +0530

    add interceptor

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,13 +1,21 @@
 import axios from "axios";
-const storedToken = localStorage.getItem("token");
-const token = JSON.parse(storedToken);
 const config = {
   withCredentials: true,
   baseURL: process.env.REACT_APP_BASE_URL,
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
 };
+
 const API = axios.create(config);
+
+API.interceptors.request.use(
+  function (config) {
+    const storedToken = localStorage.getItem("token");
+    const token = JSON.parse(storedToken);
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 export default API;
